116c5ef0190adf9c40d678e0fbf9e2269d4a2f4b
Andrew J. Felton <felton12392@gmail.com>
2022-07-05 16:04:18 -0400

    Add files via upload

---
+++ 01_build.R
@@ -0,0 +1,21 @@
+
+#setup workspace
+
+#check version
+#R.version
+
+#clearworkspace
+rm(list=ls())
+
+#load packages
+pkgs <- c("raster",'tidyverse','data.table',
+          'rstudioapi','readr')
+lapply(pkgs, library, character.only = TRUE) 
+
+# Set working directory to local directory
+current_path <- getActiveDocumentContext()$path
+setwd(dirname(current_path))
+
+#load functions
+source('02_Functions.R')
+
